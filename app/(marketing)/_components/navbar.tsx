5e3063a6d7f491bf5abfe2b94f7cbaa0fd0aced1
D0oppa <devgarcia.004@gmail.com>
2024-07-10 02:40:43 -0500

    add: NavBar

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,16 @@
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+
+export const Navbar = () => {
+  return (
+    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+          <Button>Login</Button>
+          <Button>Get Free</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "./_components/navbar";
 
 const MarketingLayout = ({
     children
@@ -6,6 +7,7 @@ const MarketingLayout = ({
 }) => {
     return (
         <div className="h-full bg-slate-100">
+            <Navbar/>
             <main className="pt-40 pb-20 bg-slate-100">
             {children}
             </main>
