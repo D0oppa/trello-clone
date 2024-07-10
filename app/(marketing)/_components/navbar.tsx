9e33e0086f231ab1922afb25a0a5b51015760c8e
D0oppa <devgarcia.004@gmail.com>
2024-07-10 02:50:01 -0500

    update: Added references for login and registration

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 
@@ -7,8 +8,12 @@ export const Navbar = () => {
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-          <Button>Login</Button>
-          <Button>Get Free</Button>
+          <Button size="sm" variant="outline" asChild>
+            <Link href="/sign-in">Login</Link>
+          </Button>
+          <Button size="sm" asChild>
+            <Link href="/sign-up"> Start Free</Link>
+          </Button>
         </div>
       </div>
     </div>
