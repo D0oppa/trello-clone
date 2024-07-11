014d33b037fc6cbf8fd1808fddc5b4fad24388bc
D0oppa <devgarcia.004@gmail.com>
2024-07-11 10:06:56 -0500

    update: routes and restrictions after login

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,30 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware({
-    
-})
+const isPublicRoute = createRouteMatcher(['/','/sign-in(.*)', '/sign-up(.*)']);
+
+export default clerkMiddleware((auth, request) =>{
+  const {userId, orgId} = auth()
+  if(!isPublicRoute(request)){
+    auth().protect();
+  }
+
+  if (userId && isPublicRoute(request)){
+    let path = "/select-org"
+    if(orgId){ path = `/organization/${orgId}`}
+    const orgSelection = new URL(path,request.url);
+    return NextResponse.redirect(orgSelection)
+  }
+
+  if(!userId && !isPublicRoute(request)){
+    return auth().redirectToSignIn({returnBackUrl: request.url})
+  }
+  if(userId && !orgId && request.nextUrl.pathname !== "/select-org"){
+    const orgSelection = new URL("/select-org",request.url);
+    return NextResponse.redirect(orgSelection)
+  }
+});
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+}
